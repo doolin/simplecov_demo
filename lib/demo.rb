a1ecbc41f8a9474029ef25afb1b9fa0a741fd9f3
dave doolin <david.doolin@gmail.com>
2023-01-07 06:43:33 -0800

    Demo class with first spec covered
    Nothing yet to show, but the toolchain is working, and the
    initial spec show 100% coverage as there is nothing in the
    class, it just instantiates.

--- lib/demo.rb
+++ lib/demo.rb
@@ -1,2 +1,7 @@
 # frozen_string_literal: true
 # SimpleCov demo
+
+# An example class for demonstrating how SimpleCov
+# reports branch and line coverage.
+class CoverageDemo
+end
--- spec/demo_spec.rb
+++ spec/demo_spec.rb
@@ -1,2 +1,10 @@
 # frozen_string_literal: true
 # Specs for simplecov demo
+
+require './lib/demo'
+
+RSpec.describe CoverageDemo do
+  example 'do something' do
+    expect(CoverageDemo.new).to_not be nil
+  end
+end
